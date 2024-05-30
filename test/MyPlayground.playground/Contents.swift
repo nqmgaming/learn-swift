28f8a5595a601ab0de3c829009b47ea262d65b03
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 06:14:39 +0700

    func

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -1,14 +1,23 @@
 import UIKit
 
-func printInstructorName(name: String){
-    print("Instructor name is \(name)")
+var isDarkMode = true
+
+if isDarkMode == true{
+    print("Dark Mode is On")
+    isDarkMode = false
+}else{
+    print("Dark Mode is Off")
+    isDarkMode = true
 }
 
-printInstructorName(name: "Nguyen Quang Minh")
-printInstructorName(name: "Lu Bich Thuy")
+var myAge = 25
+var mySistersAge = 23
 
-func add(fistNumber: Int, to SecondNumber: Int) -> Int{
-    return fistNumber + SecondNumber
-}// What is to in the function?? It is the external name of the second parameter
+if myAge > mySistersAge{
+    print("I am older than my sister")
+}else if myAge < mySistersAge{
+    print("I am younger than my sister")
+}else{
+    print("We are the same age")
+}
 
-print(add(fistNumber: 3, to: 4))
