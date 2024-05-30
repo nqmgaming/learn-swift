651a4344948a9335011e02f78da280b8a0d4b3ef
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-30 10:10:19 +0700

    Set

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -8,14 +8,18 @@ let myName = "Minh"
 var currentActiveUsers = 124
 currentActiveUsers = 1234
 
-var ages: [Int] = [19, 20, 21, 22, 23] // Array of Int
+var ages  = [12, 13, 14, 15, 16, 16]
+//var agesSet: Set<Int> = []
 
-print(ages.count) // 5
-ages.first // 19
-ages.last // 23
-ages[2] // 21
-ages.append(24) // [19, 20, 21, 22, 23, 24]
-ages.insert(34, at: 4) // [19, 20, 21, 22, 34, 23, 24]
-ages.shuffle() // [24, 21, 22, 19, 23, 34, 20]
-ages.sort() // [19, 20, 21, 22, 23, 24, 34]
-print(ages) // [19, 20, 21, 22, 23, 24, 34]
+var agesSet = Set(ages)
+
+print(agesSet)
+
+agesSet.contains(12)
+
+// set not allow duplicate value
+// set is not ordered
+// set not use append
+
+agesSet.insert(17)
+print(agesSet)
