6b46c8eeb6199adb6ecb7b623ae2fefb37b1e7a8
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-30 09:13:02 +0700

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Nguyen Quang Minh on 30/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
