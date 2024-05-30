d78e4933948b867b445747fbbf298db4d7b5d5cb
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 06:10:48 +0700

    func

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -6,3 +6,9 @@ func printInstructorName(name: String){
 
 printInstructorName(name: "Nguyen Quang Minh")
 printInstructorName(name: "Lu Bich Thuy")
+
+func add(fistNumber: Int, to SecondNumber: Int) -> Int{
+    return fistNumber + SecondNumber
+}// What is to in the function?? It is the external name of the second parameter
+
+print(add(fistNumber: 3, to: 4))
