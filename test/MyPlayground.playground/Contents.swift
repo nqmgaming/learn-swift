64b8710232fc3114d0d085ffe345357b6baed385
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-30 09:50:50 +0700

    Array

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -8,8 +8,14 @@ let myName = "Minh"
 var currentActiveUsers = 124
 currentActiveUsers = 1234
 
-var ages: [Int] = [19, 20, 21, 22, 23]
+var ages: [Int] = [19, 20, 21, 22, 23] // Array of Int
 
-print(ages.count)
-ages.first
-ages.last
+print(ages.count) // 5
+ages.first // 19
+ages.last // 23
+ages[2] // 21
+ages.append(24) // [19, 20, 21, 22, 23, 24]
+ages.insert(34, at: 4) // [19, 20, 21, 22, 34, 23, 24]
+ages.shuffle() // [24, 21, 22, 19, 23, 34, 20]
+ages.sort() // [19, 20, 21, 22, 23, 24, 34]
+print(ages) // [19, 20, 21, 22, 23, 24, 34]
