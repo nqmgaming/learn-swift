1e52dacd54b92f75a9ebda63bc3e9632570e4fa8
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 06:28:04 +0700

    for loop

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -3,9 +3,20 @@ import UIKit
 let allStars = ["James", "Davis", "Harden", "Doncic", "Leonard"]
 
 for player in allStars where player == "James" || player == "Davis"{
-//    print(player)
+    //    print(player)
+}
+
+var randomInts: [Int] = []
+
+for i in 1...10 {
+    let randomInt = Int.random(in: 1...100)
+    randomInts.append(randomInt)
+}
+
+for i in randomInts where i % 2 == 0 {
+    print(i)
 }
 
 for i in 0..<allStars.count where i % 2 == 0 {
-    print(allStars[i])
+    //    print(allStars[i])
 }
