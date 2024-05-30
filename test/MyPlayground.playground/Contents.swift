73e9b213a9ca330090ff546009b3c07b92db7b8d
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 05:57:59 +0700

    dictionary

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -1,25 +1,15 @@
 import UIKit
 
-var highScore = 0
-highScore = 10
+let devices: [String: String] = [
+    "phone"  : "iPhone",
+    "tablet" : "iPad",
+    "watch"  : "Apple Watch",
+    "laptop" : "MacBook Pro",
+] // key : value
 
-let myName = "Minh"
+devices["phone"]
+devices.count
+devices.isEmpty
+devices.keys
+devices.values
 
-var currentActiveUsers = 124
-currentActiveUsers = 1234
-
-var ages  = [12, 13, 14, 15, 16, 16]
-//var agesSet: Set<Int> = []
-
-var agesSet = Set(ages)
-
-print(agesSet)
-
-agesSet.contains(12)
-
-// set not allow duplicate value
-// set is not ordered
-// set not use append
-
-agesSet.insert(17)
-print(agesSet)
