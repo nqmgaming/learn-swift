f863e31a784e7193a0620e9957a05b24a41fdb03
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 16:54:01 +0700

    Extension

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -1,23 +1,13 @@
 import UIKit
 
-
-// Structs: Structs are value types. It means that when you pass a struct to a function or assign it to a variable, it gets copied. This is different from classes, which are reference types. When you pass a class to a function or assign it to a variable, you are passing a reference to the same object in memory.
-
-struct Developer {
-    var name: String
-    var jobTitle: String
-    var yearsOfExperience: Int
-
-    init(name: String, jobTitle: String, yearsOfExperience: Int) { // it like a constructor in Java
-        self.name = name
-        self.jobTitle = jobTitle
-        self.yearsOfExperience = yearsOfExperience
+extension String{
+    func isPalindrome() -> Bool{
+        let lowercased = self.lowercased()
+        return lowercased == String(lowercased.reversed())
     }
 }
 
-var developer = Developer(name: "John", jobTitle: "iOS Developer", yearsOfExperience: 5)
-
-var developer2 = developer
-
-developer2.name = "Jane"
-developer.name // John
+let str = "A"
+let str2 = "1d"
+print(str.isPalindrome()) // true
+print(str2.isPalindrome()) // false
