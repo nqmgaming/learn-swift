f2d85b6c94b9144826e5e1e38139e4eb1b686598
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 15:24:41 +0700

    Optionals

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -1,12 +1,40 @@
 import UIKit
 
-var counter = 10
+var ages: [Int] = []
 
-counter -= 1
+ages.sort()
 
-let greeting = "hello"
-let name = "Minh"
-print(greeting + " " + name)
+let oldest = ages.last
 
+// If let
+if let oldest = oldest {
+    print("Oldest person is \(oldest) years old")
+} else {
+    print("No one has been born yet")
+}
 
+// Nil coalescing operator
+let oldestAge = ages.last ?? 0
+print("Oldest person is \(oldestAge) years old")
+
+// Guard statement
+func printOldestPerson() {
+    guard let oldest = oldest else {
+        print("No one has been born yet")
+        return
+    }
+    print("Oldest person is \(oldest) years old")
+}
+
+printOldestPerson()
+
+// Force unwrapping
+let oldestPerson = oldest! // if oldest is nil, this will crash
+print("Oldest person is \(oldestPerson) years old")
+
+// Optional chaining
+let label = UILabel()
+label.text = "Hello, World!"
+let text = label.text?.lowercased()
+print(text)
 
