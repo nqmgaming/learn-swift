639b63ca20693446d049cd1eece8bbaf7ed5620b
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 15:13:42 +0700

    Operators

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -1,50 +1,12 @@
 import UIKit
 
-// Operators
+var counter = 10
 
+counter -= 1
 
-let valueOne: Int = 45
-let valueTwo : Int = 23
+let greeting = "hello"
+let name = "Minh"
+print(greeting + " " + name)
 
-let sum = Double(valueOne) / Double(valueTwo)
-let mod = valueOne % valueTwo
-let mul = valueOne * valueTwo
-let sub = valueOne - valueTwo
-let add = valueOne + valueTwo
-
-print(type(of: sum))
-
-
-// Comparison Operators
-
-let isEqual = valueOne == valueTwo
-
-let isNotEqual = valueOne != valueTwo
-
-let isGreater = valueOne > valueTwo
-
-let isLess = valueOne < valueTwo
-
-let isGreaterOrEqual = valueOne >= valueTwo
-
-let isLessOrEqual = valueOne <= valueTwo
-
-// Logical Operators
-
-let isTrue = true
-
-let isFalse = false
-
-let and = isTrue && isFalse
-
-let or = isTrue || isFalse
-
-let not = !isTrue
-
-// Ternary Operator
-
-let result = valueOne > valueTwo ? "Value One is greater" : "Value Two is greater"
-
-print(result)
 
 
