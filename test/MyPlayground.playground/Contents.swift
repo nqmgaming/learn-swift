b067f2fc09a4ead4ffdb793f4aff6355b2f56adc
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-31 15:11:58 +0700

    Operators

--- test/MyPlayground.playground/Contents.swift
+++ test/MyPlayground.playground/Contents.swift
@@ -1,34 +1,50 @@
 import UIKit
 
-// Switch statement
+// Operators
 
-enum WeatherType {
-    case sun
-    case cloud
-    case rain
-    case wind(speed: Int)
-    case snow
-}
 
-let weather: WeatherType = .wind(speed: 5)
+let valueOne: Int = 45
+let valueTwo : Int = 23
 
-func getWeatherInfo(weather: WeatherType) -> String {
-    switch weather {
-        case .sun:
-            return "It's sunny"
-        case .cloud:
-            return "It's cloudy"
-        case .rain:
-            return "It's raining"
-        case .wind(let speed) where speed < 10:
-            return "It's windy"
-        case .wind(let speed) where speed >= 10:
-            return "It's very windy"
-        case .snow:
-            return "It's snowing"
-        default:
-            return "Unknown weather"
-    }
-}
+let sum = Double(valueOne) / Double(valueTwo)
+let mod = valueOne % valueTwo
+let mul = valueOne * valueTwo
+let sub = valueOne - valueTwo
+let add = valueOne + valueTwo
 
-print(getWeatherInfo(weather: .rain))
+print(type(of: sum))
+
+
+// Comparison Operators
+
+let isEqual = valueOne == valueTwo
+
+let isNotEqual = valueOne != valueTwo
+
+let isGreater = valueOne > valueTwo
+
+let isLess = valueOne < valueTwo
+
+let isGreaterOrEqual = valueOne >= valueTwo
+
+let isLessOrEqual = valueOne <= valueTwo
+
+// Logical Operators
+
+let isTrue = true
+
+let isFalse = false
+
+let and = isTrue && isFalse
+
+let or = isTrue || isFalse
+
+let not = !isTrue
+
+// Ternary Operator
+
+let result = valueOne > valueTwo ? "Value One is greater" : "Value Two is greater"
+
+print(result)
+
+
